c8b6a908e478cf0da01d2b61864bee41e59df847
James Kong <fishkingsin@gmail.com>
2024-12-23 13:17:07 +0800

    Initial Commit

---
+++ CaseStudyReactiveProgramming/CaseStudyReactiveProgrammingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaseStudyReactiveProgrammingApp.swift
+//  CaseStudyReactiveProgramming
+//
+//  Created by James Kong on 23/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CaseStudyReactiveProgrammingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaseStudyReactiveProgrammingTests/CaseStudyReactiveProgrammingTests.swift
@@ -0,0 +1,16 @@
+//
+//  CaseStudyReactiveProgrammingTests.swift
+//  CaseStudyReactiveProgrammingTests
+//
+//  Created by James Kong on 23/12/2024.
+//
+
+import Testing
+
+struct CaseStudyReactiveProgrammingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
